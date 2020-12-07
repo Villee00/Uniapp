0559fe8c30b0824b851dd556c668e42b17c14ecb
karinaraikula <karina.raikula@metropolia.fi>
2020-12-07 16:46:49 +0200

    kaavio ja piirakka

---
+++ app/src/main/java/com/example/uniapp/BarChartActivity.java
@@ -0,0 +1,80 @@
+package com.example.uniapp;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+
+import java.util.ArrayList;
+
+public class BarChartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bar_chart);
+
+        //nukkumisen viikkotilasto
+        //BarEntry x ja y pitää muuttaa päivämääriksi ja syötetyiksi unen määriksi
+        //nykyset numerot testimielessä
+
+        BarChart barChart = findViewById(R.id.barChart);
+
+        //unen määrä esimerkki
+        ArrayList<BarEntry> hoursOfSleep = new ArrayList<>();
+        hoursOfSleep.add(new BarEntry(1, 8));
+        hoursOfSleep.add(new BarEntry(2, 7));
+        hoursOfSleep.add(new BarEntry(3, 5));
+        hoursOfSleep.add(new BarEntry(4, 8));
+        hoursOfSleep.add(new BarEntry(5, 6));
+        hoursOfSleep.add(new BarEntry(6, 6));
+        hoursOfSleep.add(new BarEntry(7, 6));
+
+        //unen laatu esimerkki
+        ArrayList<BarEntry> sleepQuality = new ArrayList<>();
+        sleepQuality.add(new BarEntry(1, 10));
+        sleepQuality.add(new BarEntry(2, 7));
+        sleepQuality.add(new BarEntry(3, 3));
+        sleepQuality.add(new BarEntry(4, 10));
+        sleepQuality.add(new BarEntry(5, 6));
+        sleepQuality.add(new BarEntry(6, 7));
+        sleepQuality.add(new BarEntry(7, 6));
+
+        //palkkien ulkonäkö
+        BarDataSet barDataSet = new BarDataSet(hoursOfSleep, "Hours of Sleep");
+        barDataSet.setColors(Color.BLUE);
+        barDataSet.setValueTextColor(Color.BLACK);
+        barDataSet.setValueTextSize(16f);
+
+        BarDataSet barDataSet2 = new BarDataSet(sleepQuality, "Sleep Quality");
+        barDataSet2.setColors(Color.GREEN);
+        barDataSet2.setValueTextColor(Color.BLACK);
+        barDataSet2.setValueTextSize(16f);
+
+        BarData barData = new BarData(barDataSet, barDataSet2);
+
+        //koko tilaston ulkonäkö
+        barChart.setFitBars(true);
+        barChart.setData(barData);
+        barChart.getDescription().setText("Bar Chart Example");
+        barChart.animateY(2000);
+        barChart.groupBars(0, (float) 0.1, 0);
+        barChart.setDragEnabled(true);
+
+
+        //nappi vie unen (kuukausi?) yhteenvetoon: PieChartActivity
+        findViewById(R.id.buttonPieChart).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), PieChartActivity.class));
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/uniapp/MainActivity.java
+++ app/src/main/java/com/example/uniapp/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.uniapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //nappi nukkumisen viikkohistoriaan
+        findViewById(R.id.buttonBarChart).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), BarChartActivity.class));
+            }
+        });
+
+
     }
 }
