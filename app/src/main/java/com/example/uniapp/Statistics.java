0288f0f0697f21e949e8f15ba3438f450dce5e4d
dredxster <enne.jussi@gmail.com>
2020-12-09 13:40:04 +0200

    Sekoilun mahdollinen korjaus

--- app/src/main/java/com/example/uniapp/Statistics.java
+++ app/src/main/java/com/example/uniapp/Statistics.java
@@ -2,12 +2,17 @@ package com.example.uniapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 public class Statistics extends AppCompatActivity {
+    //public static final String EXTRA = "com.example.uniapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,10 +20,17 @@ public class Statistics extends AppCompatActivity {
         setContentView(R.layout.activity_statistics);
     }
 
-    public ArrayList<String> getNotes(Calendar start, Calendar end){
-        ArrayList<String> notes = new ArrayList<String>();
+    public void viewNotes() {
+        Intent nextActivity = new Intent(Statistics.this, Notes.class);
+        startActivity(nextActivity);
+    }
+
+    public static ArrayList<String> getNotes(){
+        ArrayList<String> notes = new ArrayList<>();
 
         //Something here to get the right notes
+        /*Uni uni = new Uni(8, Calendar.getInstance(), "Hyvät unet");
+        notes.add(uni.getNote());*/
 
         return notes;
     }
