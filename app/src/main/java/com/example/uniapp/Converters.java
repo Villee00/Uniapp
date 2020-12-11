29a216903e4c590ed82878165390d67d273adaa9
Ville <ville.2000.vhk@gmail.com>
2020-12-11 02:35:07 +0200

    Muutin et toimii vain pystyssä

--- app/src/main/java/com/example/uniapp/Converters.java
+++ app/src/main/java/com/example/uniapp/Converters.java
@@ -4,7 +4,16 @@ import androidx.room.TypeConverter;
 
 import java.util.Calendar;
 
+/**
+ * Class to convert Calendar value to the long so we can store it to the local database
+ * @author Ville Haapamäki
+ */
 public class Converters {
+    /**
+     * Change an long obejct back to Calendar object
+     * @param l long that is wanted to be switch to Calendar object
+     * @return Calendar object with the value of given long
+     */
     @TypeConverter
     public static Calendar toCalendar(Long l) {
         Calendar c = Calendar.getInstance();
@@ -12,6 +21,11 @@ public class Converters {
         return c;
     }
 
+    /**
+     * Change Calendar obeject to long object so it can be stored in the local database
+     * @param c Calendar value that is wanted to change to long
+     * @return  Long obejct so it can be store to the database
+     */
     @TypeConverter
     public static Long fromCalendar(Calendar c){
         return c == null ? null : c.getTime().getTime();
